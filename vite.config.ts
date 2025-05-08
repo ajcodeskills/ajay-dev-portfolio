f2becc66b8951ccdd5e3e96d03d9f5188a47f136
ajaykumar <ajay.kumar@wrkspot.com>
2025-05-08 10:33:34 +0530

    WSS-40686 | updated the layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), eslint()],
+  plugins: [react(), tailwindcss()],
 });
